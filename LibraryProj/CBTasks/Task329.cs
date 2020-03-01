c4ea0a0fe0f3735f9c43e9171983f8c6c54ae504
Nafis Khalilov <nafis5559@gmail.com>
2020-03-01 16:46:07 +0300

    Task329 added

---
+++ LibraryProj/CBTasks/Task329.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryProj.CBTasks
+{
+    public class SignedNumber
+    {
+        public SignedNumber(int number)
+        {
+            Number = number;
+        }
+
+        public int Number { get; private set; }
+
+        public void Negate()
+        {
+            Number *= -1;
+        }
+        public override string ToString()
+        {
+            return $"{Number}";
+        }
+    }
+
+    public class Task329Solver {
+
+        public Task329Solver() { }
+
+        public IEnumerable<SignedNumber> Solve(IEnumerable<SignedNumber> numbers, int desiredSum)
+        {
+            var localCopy = numbers.ToList();
+            var i = 0;
+            var iterations = localCopy.Count * 2; // todo: think about upper bound
+            while ( i++ < iterations) {
+
+                var allSum = localCopy.Sum();
+
+                var extra = allSum - desiredSum;
+                var extraAbs = Math.Abs(extra);
+                if (extraAbs == 0)
+                {
+                    break;
+                }
+
+                var desiredNumberToNegate = localCopy.FirstOrDefault(x => x.Number == extra / 2);
+                if ( desiredNumberToNegate is null ) {
+                    desiredNumberToNegate = localCopy.OrderBy(x => x.Number).Last();
+                }
+
+                desiredNumberToNegate?.Negate(); 
+            }
+
+            return localCopy;
+        }
+    }
+
+    public static class MyExtensions {
+        public static int Sum(this IEnumerable<SignedNumber> numbers)
+        {
+            var result = numbers.Sum(x => x.Number);
+
+            return result;
+        }
+    }
+}
---
+++ TestProj/CBTaskTests/Task329Tests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Linq;
+using LibraryProj.CBTasks;
+using NUnit.Framework;
+
+namespace TestProj.CBTaskTests
+{
+    [TestFixture]
+    public class Task329Tests
+    {
+        [TestCaseSource(typeof(Task329DataProvider), "TestCases")]
+        public bool Task329_CorrectOutput(int[] input, int desiredSum)
+        {
+            // arrange
+            var myList = input.Select(x => new SignedNumber(x));
+
+
+            // act
+            var solver = new Task329Solver();
+            var result = solver.Solve(myList, desiredSum);
+
+
+            // assert
+            var actualSum = result.Sum();
+            return actualSum == desiredSum;
+        }
+
+    }
+
+    public class Task329DataProvider
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] { 4, 3, 2 }, 1).Returns(true);
+                yield return new TestCaseData(new[] { 5, 2, 1, 1 }, 1).Returns(true);
+                yield return new TestCaseData(new[] { 5, 2, 7, 1 }, 5).Returns(true);
+                yield return new TestCaseData(new[] { 5, 2, 7, 1 }, 4).Returns(false);
+                yield return new TestCaseData(new[] { 5, 3, 7, 1 }, 6).Returns(true);
+            }
+        }
+    }
+}
