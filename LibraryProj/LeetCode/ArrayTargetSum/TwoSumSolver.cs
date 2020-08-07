f589df85cae6387d79d31376e76f283944f1a8b2
rolandcheck <nafis5559@gmail.com>
2020-08-07 17:11:09 +0300

    TwoSum task from LeetCode

---
+++ LibraryProj/LeetCode/ArrayTargetSum/TwoSumSolver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace TwoSum
+{
+    /// <summary>
+    /// Solves https://leetcode.com/problems/two-sum/ task
+    /// </summary>
+    public class TwoSumSolver
+    {
+        /// <summary>
+        /// Solves Two Sum task
+        /// </summary>
+        /// <param name="nums">Array of numbers</param>
+        /// <param name="target">Target sum of two numbers from input array</param>
+        /// <returns>Array of two number indices that sum up to target</returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public int[] Solve(int[] nums, int target)
+        {
+            var bag = new Dictionary<int, int>();
+            
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var residue = target - nums[i];
+                
+                if (bag.ContainsKey(residue))
+                {
+                    return new[] {bag[residue], i};
+                } 
+                
+                bag.Add(nums[i], i);
+            }
+
+            throw new TwoSumSolverException($"There is no two numbers that sum to target {target}");
+        }
+    }
+
+    public class TwoSumSolverException : Exception
+    {
+        public TwoSumSolverException()
+        {
+        }
+
+        public TwoSumSolverException(string str) : base(str)
+        {
+        }
+        
+        public TwoSumSolverException(string str, Exception inner)
+        {
+        }
+    }
+}
---
+++ TestProj/LeetCode/ArrayTargetSumSolverShould.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using TwoSum;
+using NUnit.Framework;
+
+namespace TestProj.LeetCode
+{
+    [TestFixture]
+    public class ArrayTargetSumSolverShould
+    {
+        private TwoSumSolver _solver;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _solver = new TwoSumSolver();
+        }
+
+
+        [TestCaseSource(nameof(CorrectTestCases))]
+        public int[] ReturnCorrectResult(int[] nums, int target)
+        {
+            return _solver.Solve(nums, target);
+        }
+
+        private static IEnumerable<TestCaseData> CorrectTestCases()
+        {
+            yield return new TestCaseData(new[] {2, 7, 11, 15}, 9).Returns(new[] {0, 1});
+            yield return new TestCaseData(new[] {2, 7, 11, 15}, 13).Returns(new[] {0, 2});
+            yield return new TestCaseData(new[] {2, 7, 11, 15}, 22).Returns(new[] {1, 3});
+        } 
+    }
+}
